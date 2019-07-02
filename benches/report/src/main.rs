c4c1e8559be78c021d7e8383afebc1e09bd75060
Michiel De Muynck <michieldemuynck@gmail.com>
2019-07-02 21:53:25 +0200

    begin refactor to index rather than slices

--- benches/report/src/main.rs
+++ benches/report/src/main.rs
@@ -40,7 +40,10 @@ set style fill solid 0.5
 
 set arrow from -0.5,1 to 8,1 nohead
 
-plot \"./graphs/{test_type}.txt\" using 1:3:4 with boxes lc rgb var notitle, \"\"  using ($1):($3+0.2):(sprintf(\"%3.2f\",$3)) with labels font \"Arial,8\" rotate by 90 notitle", test_type=test_type, ymax=ymax);
+plot \"./graphs/{test_type}.txt\" using 1:3:4 with boxes lc rgb var notitle, \"\"  using ($1):($3+0.2):(sprintf(\"%3.2f\",$3)) with labels font \"Arial,8\" rotate by 90 notitle
+
+set output
+", test_type=test_type, ymax=ymax);
         
         let file = File::create(Path::new(&format!("./graphs/{}.gp", test_type))).unwrap();
         let mut writer = BufWriter::new(file);
@@ -55,6 +58,8 @@ plot \"./graphs/{test_type}.txt\" using 1:3:4 with boxes lc rgb var notitle, \"\
 }
 
 fn main() {
+    std::fs::create_dir_all("./graphs").unwrap();
+
     let algos = ["ironed", "bump_tree", "indextree", "ego_tree", "vec_tree", "naive_tree", "id_tree"];
 
     plot_test_type("make", &algos, 10.);
